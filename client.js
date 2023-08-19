a10db1707f00276d0b82eb3c2469957df9d3edbc
EmmaP <35646411+emmap88@users.noreply.github.com>
2023-08-19 03:03:49 -0400

    Update

--- client.js
+++ client.js
@@ -1,18 +1,22 @@
 const net = require("net");
 
 // establishes a connection with the game server
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({ //Node's net library (specifically, the createConnection function) to create an object named conn in the code above. This establishes a connection to the game server.
     host: '10.0.2.15', // IP address here,
     port: 50541,// PORT number here,
   });
-  conn.on("connect", (data) => {
-    // code that does something when the connection is first established
-    console.log("you ded cuz you idled ", data);
-  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("Successfully connected to game server ");
+    conn.write("Name: EMP");
+  });
+
+
 
   return conn; //conn object that Node returned to you represents the connection that you have with the server.
 
