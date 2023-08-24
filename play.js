e6d34f3257b2006028ce96ba5289ac04406ea758
emmap88 <emmap8@hotmail.com>
2023-08-24 12:05:24 -0400

    Update play.js

--- play.js
+++ play.js
@@ -2,4 +2,16 @@ const { connect } = require('./client');
 
 console.log('Connecting ...');
 
+connect();
+
+// setup interface to handle user input from stdin
+// stdin listen and react to keyboard input
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
 module.exports = { connect };
