33026accabd25ad679070624d4c477c28009a981
emmap88 <emmap8@hotmail.com>
2023-09-12 17:06:54 -0400

    Update input.js

--- input.js
+++ input.js
@@ -1,9 +1,5 @@
-// const { connect } = require('./client');
-
-// console.log('Connecting ...');
-
-// connect();
 let connection;
+let currentDirection = null; 
 // setup interface to handle user input from stdin
 // stdin listen and react to keyboard input
 const setupInput = function(conn) {
@@ -18,36 +14,56 @@ const setupInput = function(conn) {
   return stdin;
 };
 const handleUserInput = (key) => {
-  console.log(key);
-  console.log(connection);
 
-  if (key === 'w') {
-    connection.write("Move: up");
+  if (key === 'w' && currentDirection !== 'down') {
+    connection.write('Move: up');
+    currentDirection = 'up';
   }
-  if (key === 'a') {
-    connection.write("Move: left");
+
+  if (key === 'a' && currentDirection !== 'right') {
+    connection.write('Move: left');
+    currentDirection = 'left';
   }
-  if (key === 's') {
-    connection.write("Move: down");
+
+  if (key === 's' && currentDirection !== 'up') {
+    connection.write('Move: down');
+    currentDirection = 'down'; 
   }
-  if (key === 'd') {
-    connection.write("Move: right");
+
+  if (key === 'd' && currentDirection !== 'left') {
+    connection.write('Move: right');
+    currentDirection = 'right';
   }
   if (key === '\u0003') {
-    process.exit(); // control c
+    process.exit(); // Ctrl+C to quit
   }
 
+  if (key === 'k') {
+    connection.write('Say: Oh..!');
+    setTimeout(() => {
+      connection.write('Say: Oops!');
+    }, 500);
+    setTimeout(() => {
+      connection.write('Say: Ahhh!');
+    }, 1000);
+    setTimeout(() => {
+      connection.write('Say: The Ennnnd!');
+    }, 2000);
+    setTimeout(() => {
+      connection.write('Say: What a Wooorld!');
+    }, 3000);
   
-  connection.write("Move: down");
-  setTimeout(() => {
-    connection.write("Move: up");
-  }, 500);
+  }
+//   connection.write("Move: down");
+//   setTimeout(() => {
+//     connection.write("Move: up");
+//   }, 500);
 
-  setTimeout(() => {
-    connection.write(("Move: right"));
-  }, 1000);
-  setTimeout(() => {
-    connection.write("Move: left");
-  }, 1500);
-};
+//   setTimeout(() => {
+//     connection.write(("Move: right"));
+//   }, 1000);
+//   setTimeout(() => {
+//     connection.write("Move: left");
+//   }, 1500);
+ };
 module.exports = { setupInput };
