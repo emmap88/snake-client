a65c77af81a76b9b489eb2e8e0ecf546a6e1c346
emmap88 <emmap8@hotmail.com>
2023-09-13 18:10:36 -0400

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,11 @@
+// Define constant for server IP address
+const IP = "localhost";
+
+// Define constant for server port number
+const PORT = 50541;
+
+// Export constants to make available in other modules
+module.exports = {
+  IP,
+  PORT,
+};
