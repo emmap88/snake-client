f1627275551fb356950cc6193b8fa5f33a7fa8db
emmap88 <emmap8@hotmail.com>
2023-09-13 19:38:03 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,75 +1,34 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({ //Node's net library (specifically, the createConnection function) to create an object named conn in the code above. This establishes a connection to the game server.
-    host: 'localhost', // IP address here,
-    port: 50541,// PORT number here,
+// Create a function to establish a connection with the game server
+const connect = function () {
+
+  // Use Node's net library to create a connection object
+  const conn = net.createConnection({
+    host: IP, // IP address of game server
+    port: PORT // Port number of game server
   });
 
-  // interpret incoming data as text
+  // Set encoding to UTF-8 for incoming data
   conn.setEncoding("utf8");
 
-  
+  // When connection is established, log a message
   conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("Successfully connected to game server ");
-    conn.write("Name: EMP");
-  
-    // conn.write("Move: down");
-    //   setTimeout(() => {
-    //     conn.write("Move: up");
-    //   }, 500);
+    console.log("Successfully connected to game server");
 
-    //   setTimeout(() => {
-    //     conn.write(("Move: right"));
-    //   }, 1000);
-    //   setTimeout(() => {
-    //     conn.write("Move: left");
-    //   }, 1500);
- 
+    // Send name to server after connecting
+    conn.write("Name: EMP");
   });
 
+  // Log any incoming data from server
   conn.on("data", (data) => {
     console.log(data);
   });
 
-  // conn.on("close", () => {
-  //   console.log("Connection closed");
-  // });
-
-  // conn.on("error", (err) => {
-  //   console.log("Error: ", err);
-  // });
-
-  // conn.on("end", () => {
-  //   console.log("Connection ended");
-  // });
-
-  // conn.on("drain", () => {
-  //   console.log("Connection drained");
-  // });
-
-  
-  // let direction = "up";
-
-  // setInterval(() => {
-  //   conn.write(`Move: ${direction}`);
-  
-  //   // cycle through directions
-  //   if (direction === "up") direction = "left";
-  //   else if (direction === "left") direction = "down";
-  //   else if (direction === "down") direction = "right";
-  //   else if (direction === "right") direction = "up";
-
-  // }, 500);
-
-
-  return conn; //conn object that Node returned to you represents the connection that you have with the server.
-
-  // The conn object is full of useful methods and properties that can now be used to interact with the server!
+  // Return the connection object
+  return conn;
 };
 
+// Export the connect function
 module.exports = { connect };
-// console.log("Connecting ...");
-//connect();
