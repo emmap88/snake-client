7d8d4fe3b6d41830c4cf202aa6097f8469e6e846
emmap88 <emmap8@hotmail.com>
2023-09-13 18:10:40 -0400

    Update play.js

--- play.js
+++ play.js
@@ -1,7 +1,12 @@
+// Import the connect function from the client module
 const { connect } = require("./client");
+
+// Import the setupInput function from the input module
 const { setupInput } = require("./input");
 
+// Log a message that we are connecting
 console.log("Connecting ...");
-//connect();
 
+// Call the connect function to connect to the server
+// Pass the returned connection to the setupInput function
 setupInput(connect());
